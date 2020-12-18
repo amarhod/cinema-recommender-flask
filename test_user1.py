1077d28297f73fc1f00a1a7bbb24100c458d82db
callmeGoldenboy <natanteferi@gmail.com>
2020-12-18 20:57:53 +0100

    added a user1 test file

---
+++ test_user1.py
@@ -0,0 +1,54 @@
+import database_handler as db
+
+
+my_movies = [{'Actors': ' George Mackay, Dean-Charles Chapman, Richard Madden, Benedict '
+            'Cumberbatch, Colin Firth, Mark Strong',
+  'Date': ' 31 jan 2020',
+  'Description': 'Två brittiska soldater får i uppdrag att ta sig långt in '
+                 'bakom fiendelinjen för att varna ett regemente för ett '
+                 'bakhåll som tyskarna planerar.',
+  'Directors': ' Sam Mendes',
+  'Genre': 'Drama, Krig',
+  'Original_language': ' Engelska ',
+  'Original_title': ' 1917'},
+ {'Actors': ' Christos Loulis, Ulrich Tukur',
+  'Date': ' 10 jul 2020',
+  'Description': 'I kölvattnet av börskraschen som startade i USA 2007 är '
+                 'Grekland 2015 ett land på ruinens brant. Bakom lyckta dörrar '
+                 'försöker landets finansminister Gianis Varoufakis rädda vad '
+                 'som räddas kan genom hårda åtstramningsförslag. Allt för att '
+                 'Grekland inte ska knuffas ut ur EU och ned i en evig '
+                 'skuldfälla...',
+  'Directors': ' Costa-Gavras ',
+  'Genre': 'Drama, Thriller',
+  'Original_language': ' Franska,  Tyska,  Engelska,  Grekiska ',
+  'Original_title': ' Adults in the Room'},
+ {'Actors': ' Will Smith, Martin Lawrence',
+  'Date': ' 17 jan 2020',
+  'Description': 'Poliserna Mike Lowrey och Marcus Burnett är tillbaka en '
+                 'sista gång i "Bad Boys for Life".',
+  'Directors': ' Adil El Arbi, Bilall Fallah',
+  'Genre': 'Action, Komedi',
+  'Original_language': ' Engelska ',
+  'Original_title': ' Bad Boys for Life'},
+ {'Actors': ' Christian Bale, Michael Caine, Ken Watanabe, Liam Neeson, Katie '
+            'Holmes, Gary Oldman, Rutger Hauer, Morgan Freeman',
+  'Date': ' 24 jun 2020',
+  'Description': 'Den ursprungliga berättelsen om hur Bruce Wayne bestämmer '
+                 'sig för det goda och sitt alter ego Batman. Bruce Wayne ser '
+                 'sina föräldrar mördas och vill hämnas på förövaren. Men - '
+                 'han inser att hämnd inte löser något, utan blir i stället '
+                 'Batman.',
+  'Directors': ' Christopher Nolan',
+  'Genre': 'Action, Äventyr',
+  'Original_language': ' Engelska ',
+  'Original_title': ' Batman Begins'}]
+def test_user(name="user2"):
+  handler = db.DatabaseHandler(name)
+  handler.create_table(name)
+  handler.print_table()
+  handler.close_connection()
+
+
+if __name__ == "__main__":
+  test_user()
