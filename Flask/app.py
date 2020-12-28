b21566968bba178db63ccefe2d6deb258df3d9ec
Amar Hodzic <amar_hodzic@hotmail.com>
2020-12-28 17:35:46 +0100

    Fixed app.py to display recommendation list in correct order. Also removed some HTML pages and fixed others.

--- Flask/app.py
+++ Flask/app.py
@@ -1,13 +1,21 @@
 import os
 import pprint
-from flask import Flask, render_template, url_for, flash, redirect
-from BioFilmer.Flask.forms import RegistrationForm, LoginForm
-from BioFilmer.movies import get_movie_list
+from flask import Flask, render_template, url_for, flash, redirect, request
+from BioFilmer.Flask.forms import RegistrationForm, LoginForm, FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from BioFilmer.movies import get_movie_list, match_movie
 from BioFilmer.recommender import get_recommendations
 from BioFilmer.database_handler import DatabaseHandler
 SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+
+
+class MyForm(FlaskForm):
+    name = StringField('Title', validators=[DataRequired()])
+    rating = IntegerField('Rating', validators=[DataRequired()])
 
 movies_list = get_movie_list()
 @app.route('/')
@@ -15,32 +23,24 @@ movies_list = get_movie_list()
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/signup', methods=['GET','POST'])
-def signup():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
-        return redirect(url_for('home'))
-    return render_template('signup.html', title='Sign Up', form=form)
+@app.route('/history', methods=['GET','POST'])
+def add_to_history():
+    if request.method == 'POST':
+        title = request.form['name']
+        rating = request.form['rating']
+        #print("POSTing title: " + title + " with rating: " + rating)
+        movie = match_movie(title, movies_list)
+        movie['Rating'] = rating
+        with DatabaseHandler("user1") as db:
+            db.store(movie)
+    return redirect(url_for('recommendations'))
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.username.data == 'user2' and form.password.data == 'user123':
-            flash(f'You have been logged in!','success')
-            return redirect(url_for('home'))
-        else:
-            flash(f'Log in unsuccessful.','danger')
-    return render_template('login.html',title='Log In', form=form)
-
-@app.route('/account')
-def account():
-    return render_template('account.html', title='Account')
-
-@app.route('/movies')
+@app.route('/movies', methods=['GET','POST'])
 def movies():
-    return render_template('movies.html',title='Movies', movies=movies_list)
+    form = MyForm()
+    if form.validate_on_submit():
+        flash(f'Rating added for {form.name.data}!','success')
+    return render_template('movies.html',title='Movies', movies=movies_list, form=form)
 
 @app.route('/recommendations')
 def recommendations():
@@ -48,24 +48,15 @@ def recommendations():
     final_recommendations = []
     with DatabaseHandler('user1') as handler:
         movies_seen = handler.read_all_rows()
-    #     movies_seen = [{'Actors': ' Christian Bale, Michael Caine, Ken Watanabe, Liam Neeson, Katie '
-    #             'Holmes, Gary Oldman, Rutger Hauer, Morgan Freeman',
-    #     'Date': ' 24 jun 2020',
-    #     'Description': 'Den ursprungliga berättelsen om hur Bruce Wayne bestämmer '
-    #                  'sig för det goda och sitt alter ego Batman. Bruce Wayne ser '
-    #                  'sina föräldrar mördas och vill hämnas på förövaren. Men - '
-    #                  'han inser att hämnd inte löser något, utan blir i stället '
-    #                  'Batman.',
-    #   'Directors': ' Christopher Nolan',
-    #   'Genre': 'Action, Äventyr',
-    #   'Original_language': ' Engelska ',
-    #   'Original_title': ' Batman Begins'}]
-        pprint.pprint(movies_seen)
-        recommended_movies = get_recommendations(movies_seen)
-        #from the movies list in theater, extract the indexes that match with the ones that the get recommendations returned 
-        for i in range(0,len(movies_list)):
-            if movies_list[i][0] in recommended_movies[1]:
-                final_recommendations.append(movies_list[i])
+        #pprint.pprint(movies_seen)
+        recommended_movies_indexes = get_recommendations(movies_seen)[1]
+        #from the movies list in theater, extract the indexes that match with the ones that the get recommendations returned
+        for i in range(len(recommended_movies_indexes)):
+            for j in range(len(movies_list)):
+                if recommended_movies_indexes[i] == movies_list[j][0]:
+                    final_recommendations.append(movies_list[j])
+            # if movies_list[i][0] in recommended_movies[1]:
+            #     final_recommendations.append(movies_list[i])
     return render_template('recommendations.html',title='Recommended',movies=final_recommendations)
 if __name__ == '__main__':
     app.run(debug=True)
