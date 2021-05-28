6f6f1f7018a5bdf1fcc7eaac76cabf4d91f69f5a
Amar Hodzic <amarh@kth.se>
2021-05-28 16:19:34 +0200

    feat: Added multi-agent system file to initialize other agents

--- src/agents/mas.py
+++ src/agents/mas.py
@@ -1,6 +1,13 @@
+import database_handler as db
 from scraper import scraper 
+from website import app
+from recommender import recommender
 
 
 if __name__ == '__main__':
     scraper = scraper.WebScraperAgent()
     scraper.start()
+    database = db.DatabaseHandler()
+    database.start()
+    website = app.Website()
+    website.start()
